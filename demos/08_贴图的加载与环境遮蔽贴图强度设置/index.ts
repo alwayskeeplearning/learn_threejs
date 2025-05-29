d23e3a10278584d1a194e843370696c41bef3ba5
enforcer.a06 <enforcer.a06@gmail.com>
2025-05-29 19:14:50 +0800

    feat: wip 贴图学习

---
+++ demos/08_贴图的加载与环境遮蔽贴图强度设置/index.ts
@@ -0,0 +1,91 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
+// 创建场景
+const scene = new THREE.Scene();
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(
+  75, // 视角
+  // 宽高比
+  window.innerWidth / window.innerHeight,
+  // 近截面
+  0.1,
+  // 远截面
+  1000,
+);
+
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// 创建纹理加载器
+const textureLoader = new THREE.TextureLoader();
+// 加载贴图
+const texture = textureLoader.load('/static/textures/watercover/CityNewYork002_COL_VAR1_1K.png');
+// 加载ao贴图
+const aoTexture = textureLoader.load('/static/textures/watercover/CityNewYork002_AO_1K.jpg');
+
+// 创建平面几何体
+const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  map: texture,
+  // 允许透明
+  transparent: true,
+  // 设置ao贴图
+  aoMap: aoTexture,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+scene.add(plane);
+
+// 设置相机位置
+camera.position.z = 2;
+camera.position.y = 1;
+camera.position.x = 1;
+// 设置相机朝向(看向原点)
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+// 渲染
+renderer.render(scene, camera);
+
+// 添加世界坐标轴
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 添加轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼
+controls.enableDamping = true;
+// 设置阻尼系数
+controls.dampingFactor = 0.25;
+// 设置是否启用缩放
+controls.enableZoom = true;
+// 设置是否启用平移
+controls.enablePan = true;
+
+// 动画
+function animate() {
+  requestAnimationFrame(animate);
+  // 更新轨道控制器
+  controls.update();
+  // 渲染
+  renderer.render(scene, camera);
+}
+
+animate();
+
+// 监听窗口大小变化
+window.addEventListener('resize', () => {
+  // 更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 更新相机
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+const gui = new GUI();
+gui.add(planeMaterial, 'aoMapIntensity').min(0).max(1).step(0.1).name('环境遮蔽贴图强度');
