abd1ffb3e1b9df8e5c9dd2697dfbb62513f8018e
enforcer.a06 <enforcer.a06@gmail.com>
2025-06-16 18:51:40 +0800

    feat: wip gsap动画基本使用

---
+++ demos/49_Gsap动画库基本使用与原理/index.ts
@@ -0,0 +1,86 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min';
+import gsap from 'gsap';
+
+// 创建场景
+const scene = new THREE.Scene();
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(
+  75, // 视角
+  // 宽高比
+  window.innerWidth / window.innerHeight,
+  // 近截面
+  0.1,
+  // 远截面
+  1000,
+);
+
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer({
+  antialias: true, // 抗锯齿
+  stencil: true, // 模板缓冲区
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// 设置相机位置
+camera.position.z = 10;
+camera.position.y = 3;
+camera.position.x = 0;
+// 设置相机朝向(看向原点)
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+// 渲染
+renderer.render(scene, camera);
+
+// 添加世界坐标轴
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 添加轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼
+controls.enableDamping = true;
+// 设置阻尼系数
+controls.dampingFactor = 0.25;
+// 设置是否启用缩放
+controls.enableZoom = true;
+
+// 设置是否启用平移
+controls.enablePan = false;
+// 设置控制器目标
+controls.target.set(0, 0, 0);
+
+// 动画
+function animate() {
+  // 更新轨道控制器
+  controls.update();
+  // 渲染
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+// 监听窗口大小变化
+window.addEventListener('resize', () => {
+  // 更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 更新相机
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const gui = new GUI();
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(boxMesh);
+
+gsap.to(boxMesh.position, { x: 5, duration: 5, ease: 'power1.inOut' });
+gsap.to(boxMesh.rotation, { x: 2 * Math.PI, duration: 5 });
